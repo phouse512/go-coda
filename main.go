23fc47e250b519bd5387edc595205d75cac8fe39
phouse512 <philiphouse2015@u.northwestern.edu>
2019-02-12 22:52:30 -0600

    logging coda body

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package coda
 
 import (
+	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"net/url"
 )
@@ -28,6 +30,7 @@ func (c *Client) ListDocs() ([]Document, error) {
 	u := c.BaseURL.ResolveReference(rel)
 	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
+		log.Print("Unable to create new request.")
 		return nil, err
 	}
 	req.Header.Set("Accept", "application/json")
@@ -35,11 +38,18 @@ func (c *Client) ListDocs() ([]Document, error) {
 
 	resp, err := c.HttpClient.Do(req)
 	if err != nil {
+		log.Print("Status: ", resp.Status)
+		log.Print("Body: ", resp.Body)
 		return nil, err
 	}
 
 	defer resp.Body.Close()
 	var docs []Document
 	err = json.NewDecoder(resp.Body).Decode(&docs)
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	s := buf.String()
+	log.Print(s)
 	return docs, err
 }
