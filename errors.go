5a674531e9abd33fd237684f75dff940ba44274d
phouse512 <philiphouse2015@u.northwestern.edu>
2019-02-16 21:11:07 -0600

    working on real errors

---
+++ errors.go
@@ -0,0 +1,70 @@
+package coda
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type ErrorResponse struct {
+	StatusCode    int    `json:"statusCode"`
+	StatusMessage string `json:"statusMessage"`
+	Message       string `json:"message"`
+}
+
+func buildError(resp *http.Response) error {
+	var errResp ErrorResponse
+	err := json.NewDecoder(resp.Body).Decode(&errResp)
+
+	if err != nil {
+		log.Print("Unable to deserialize Coda Error object.")
+		return err
+	}
+
+	switch errResp.StatusCode {
+	case 400:
+		return InvalidRequestError{err: errResp.StatusMessage}
+	case 401:
+		return InvalidTokenError{err: errResp.StatusMessage}
+	case 429:
+		return RateLimitError{err: errResp.StatusMessage}
+	default:
+		return ApiError{err: errResp.StatusMessage}
+	}
+}
+
+// Standard Bad Request 400 Error
+type InvalidRequestError struct {
+	err string
+}
+
+func (e *InvalidRequestError) Error() string {
+	return fmt.Sprintf("Invalid request: %s", e.err)
+}
+
+// Invalid API Token
+type InvalidTokenError struct {
+	err string
+}
+
+func (e *InvalidTokenError) Error() string {
+	return fmt.Sprintf("Invalid token: %s", e.err)
+}
+
+// Rate Limit Error
+type RateLimitError struct {
+	err string
+}
+
+func (e *RateLimitError) Error() string {
+	return fmt.Sprintf("Rate limited: %s", e.err)
+}
+
+type ApiError struct {
+	err string
+}
+
+func (e *ApiError) Error() string {
+	return fmt.Sprintf("Coda API Error: %s", e.err)
+}
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package coda
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"path"
 )
 
 type Client struct {
@@ -14,42 +17,101 @@ type Client struct {
 	HttpClient *http.Client
 }
 
-type Document struct {
-	id           string `json:"id"`
-	documentType string `json:"type"`
-	href         string `json:"href"`
-	browserLink  string `json:"href"`
-	name         string `json:"href"`
-	owner        string `json:"email"`
-	createdAt    string `json:"createdAt"`
-	updatedAt    string `json:"updatedAt"`
+type GetDocumentResponse struct {
+	Document Document
 }
 
-func (c *Client) ListDocs() ([]Document, error) {
-	rel := &url.URL{Path: "/docs"}
+type ListDocumentsResponse struct {
+	Documents []Document `json:"items"`
+}
+
+type Document struct {
+	Id           string `json:"id"`
+	DocumentType string `json:"type"`
+	Href         string `json:"href"`
+	BrowserLink  string `json:"browserLink"`
+	Name         string `json:"name"`
+	Owner        string `json:"email"`
+	CreatedAt    string `json:"createdAt"`
+	UpdatedAt    string `json:"updatedAt"`
+}
+
+func (c *Client) newRequest(method, methodPath string, body interface{}) (*http.Request, error) {
+	rel := &url.URL{Path: methodPath}
+	rel.Path = path.Join(c.BaseURL.Path, rel.Path)
 	u := c.BaseURL.ResolveReference(rel)
-	req, err := http.NewRequest("GET", u.String(), nil)
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
 	if err != nil {
-		log.Print("Unable to create new request.")
 		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
 	}
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("User-Agent", c.UserAgent)
 
+	return req, nil
+}
+
+func (c *Client) do(req *http.Request, v interface{}) (*http.Response, error) {
 	resp, err := c.HttpClient.Do(req)
 	if err != nil {
-		log.Print("Status: ", resp.Status)
-		log.Print("Body: ", resp.Body)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		return resp, buildError(resp)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(v)
+	return resp, err
+}
+
+func (c *Client) GetDoc(id string) (GetDocumentResponse, error) {
+	docPath := fmt.Sprintf("/docs/%s", id)
+	req, err := c.newRequest("GET", docPath, nil)
+	if err != nil {
+		log.Print("Unable to create new request.")
+		return GetDocumentResponse{}, err
+	}
+
+	var document Document
+	resp, err := c.do(req, &document)
+	if err != nil {
+		log.Print("Unable to make request.")
+		return GetDocumentResponse{}, err
+	}
+	log.Print("Received status: ", string(resp.StatusCode))
+	var response = GetDocumentResponse{Document: document}
+	return response, err
+}
+
+func (c *Client) ListDocs() ([]Document, error) {
+	docPath := "/docs"
+	req, err := c.newRequest("GET", docPath, nil)
+	if err != nil {
+		log.Print("Unable to create new request.")
 		return nil, err
 	}
 
-	defer resp.Body.Close()
-	var docs []Document
-	err = json.NewDecoder(resp.Body).Decode(&docs)
+	var documentsResponse ListDocumentsResponse
+	_, err = c.do(req, &documentsResponse)
+	if err != nil {
+		log.Print("Unable to make request.")
+		return nil, err
+	}
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(resp.Body)
-	s := buf.String()
-	log.Print(s)
-	return docs, err
+	return documentsResponse.Documents, err
 }
