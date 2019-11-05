090b5965626550a8eb40f9529aa9d10da64261b4
phouse512 <philiphouse2015@u.northwestern.edu>
2019-11-04 21:41:07 -0600

    implement account + misc

--- account.go
+++ account.go
@@ -1,8 +1,25 @@
 package coda
 
-type User struct {
-	Name    string `json:"name"`
-	LoginId string `json:"loginId"`
-	Type    string `json:"type"`
-	Href    string `json:"href"`
+import (
+	"fmt"
+	"log"
+)
+
+type UserInfoResponse struct {
+	Name      string `json:"name"`
+	LoginId   string `json:"loginId"`
+	Type      string `json:"type"`
+	Scoped    bool   `json:"scoped"`
+	TokenName string `json:"tokenName"`
+	Href      string `json:"href"`
+}
+
+func (c *Client) GetUserInfo() (UserInfoResponse, error) {
+	docPath := fmt.Sprintf("whoami")
+	var userResp UserInfoResponse
+	err := c.apiCall("GET", docPath, nil, &userResp)
+	if err != nil {
+		log.Print("Unable to get user info with error.")
+	}
+	return userResp, err
 }
